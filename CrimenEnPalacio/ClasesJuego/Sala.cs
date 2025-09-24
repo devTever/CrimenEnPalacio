5fff27ca956b969421e0beac176cb924bead75cc
Tever <76785084+devTever@users.noreply.github.com>
2025-09-24 19:12:21 +0200

    Update Sala.cs

--- CrimenEnPalacio/ClasesJuego/Sala.cs
+++ CrimenEnPalacio/ClasesJuego/Sala.cs
@@ -1,1 +1,12 @@
-
+namespace CrimenEnPalacio
+{
+    // Clase Sala
+    class Sala
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public Objeto Objeto { get; set; }
+        public Personaje Personaje { get; set; }
+        public List<string> Salidas { get; set; } = new List<string>();
+    }
+}
